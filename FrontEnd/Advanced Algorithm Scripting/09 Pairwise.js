b085b8985df8ff08ef951980279b511ee2c23ec7
ThiagoMArc <tf.arcanjo@gmail.com>
2018-02-27 19:06:06 -0300

    Advanced 09 Refactored

--- FrontEnd/Advanced Algorithm Scripting/09 Pairwise.js
+++ FrontEnd/Advanced Algorithm Scripting/09 Pairwise.js
@@ -1,25 +1,20 @@
 
 function pairwise(arr, arg) {
   
-  let pidx = [];
   let i,j;
   let sum = 0;
   
   for(i=0; i < arr.length; i++){
     let trgt = arr.indexOf(arg-arr[i]);
     if(trgt !== -1 && trgt !== i){
-      pidx.push(i,trgt);
+      sum += trgt + i;
       //removing the pair
       arr.splice(i,1,'visited');
       arr.splice(trgt,1,'visited');
     }
     
   }
-  
-  for(j=0; j < pidx.length; j++){
-    sum += pidx[j];
-  }
-  
+    
   return sum;
 }
 
