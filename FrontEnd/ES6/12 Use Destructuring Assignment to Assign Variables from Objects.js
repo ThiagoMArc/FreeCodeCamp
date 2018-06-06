807cf214113db55587e12d023daebb9572a24e2b
ThiagoMArc <tf.arcanjo@gmail.com>
2018-06-06 11:33:58 -0300

    ES6 Excercises

---
+++ FrontEnd/ES6/12 Use Destructuring Assignment to Assign Variables from Objects.js
@@ -0,0 +1,13 @@
+function getLength(str) {
+    "use strict";
+  
+    // change code below this line
+    const length = str.length;
+    const {length:len} = str;
+    // change code above this line
+  
+    return len; // you must assign length to len in line
+  
+  }
+  
+  console.log(getLength('FreeCodeCamp'));
